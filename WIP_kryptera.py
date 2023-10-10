6d6be96948959b7e1c6127eaa4c89a8d86b9ff46
Fresh Salad <146735584+freshestsalad@users.noreply.github.com>
2023-10-10 17:55:13 +0300

    Update WIP_kryptera.py

--- WIP_kryptera.py
+++ WIP_kryptera.py
@@ -1,11 +1,50 @@
-import random
-
 #(ord('A') return 65
 #(chr(90) return Z
 
+repeat = True
 
+def cryptMessage():
+    newMessage = ""
+    oldMessage = input("Meddelandet du vill kryptera: " + "\n")
+    while True:
+        try:
+            key = ord(input("Ange nyckeln: " + "\n"))
+        except TypeError:
+            print("Nyckeln bör vara 1 tecken!" + "\n")
+            continue
+        else:
+            break
 
-while True:
+    for oldCharacter in oldMessage:
+        if oldCharacter.isupper():
+            newCharacter = chr((ord(oldCharacter) + key - 65) % 26 + 65)
+        elif oldCharacter.islower():
+            newCharacter = chr((ord(oldCharacter) + key - 97) % 26 + 97)
+        newMessage += newCharacter
+    print(newMessage)
+
+def decryptMessage():
+    oldMessage = ""
+    newMessage = input("Meddelandet du vill dekryptera: " + "\n")
+    while True:
+        try:
+            key = ord(input("Ange nyckeln: " + "\n"))
+        except TypeError:
+            print("Nyckeln bör vara 1 tecken!" + "\n")
+            continue
+        else:
+            break
+        
+    for newCharacter in newMessage:
+        if newCharacter.isupper():
+            oldCharacter = chr((ord(newCharacter) - key - 65) % 26 + 65)
+        elif newCharacter.islower():
+            oldCharacter = chr((ord(newCharacter) - key - 97) % 26 + 97)
+        oldMessage += oldCharacter
+    print(oldMessage)
+
+while repeat:
+    while True:
         try:
             chooseTask = int(input("Vad vill du göra?" + "\n" + "1: Kryptera ett meddelande" + "\n" + "2: Dekryptera ett meddelande" + "\n"))
         except ValueError:
@@ -16,32 +55,12 @@ while True:
             continue
         else:
             break
+    
+    if chooseTask == 1:
+        cryptMessage()
+    elif chooseTask == 2:
+        decryptMessage()
 
-def cryptMessage():
-    newMessage = ""
-    oldMessage = input("Meddelandet du vill kryptera: " + "\n")
-    key = input("Ange nyckeln: " + "\n")
-
-    for oldCharacter in oldMessage:
-        if oldCharacter.isupper():
-            newCharacter = chr((ord(oldCharacter) + ord(key) - 65) % 26 + 65)
-        elif oldCharacter.islower():
-            newCharacter = chr((ord(oldCharacter) + ord(key) - 97) % 26 + 97)
-        newMessage += newCharacter
-    print(newMessage)
-
-def decryptMessage():
-    oldMessage = ""
-    newMessage = input("Meddelandet du vill dekryptera: " + "\n")
-    key = input("Ange nyckeln: " + "\n")
-
-    for newCharacter in newMessage:
-        if newCharacter.isupper():
-            oldCharacter = chr((ord(newCharacter) - ord(key) - 65) % 26 + 65)
-        elif newCharacter.islower():
-            oldCharacter = chr((ord(newCharacter) - ord(key) - 97) % 26 + 97)
-        oldMessage += oldCharacter
-    print(oldMessage)
-
-cryptMessage()
-decryptMessage()
+    repeatEnd = input('Skriv "sluta" för att sluta' + "\n")    #Frågar användaren ifall hen vill sluta spelet
+    if repeatEnd.lower() == "sluta":                     #Inputen "Sluta" (oberoende av kapitalisering) stoppar "while repeat" loopen
+        repeat = False
